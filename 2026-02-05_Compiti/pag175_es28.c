5103bbc73ba2d4fc91ba006351a5da5df5408075
Personale <fazio.rares.2023@itisgalilei.it>
2026-02-08 17:14:37 +0100

    Aggiunto esercizi febbraio 2026

---
+++ 2026-02-05_Compiti/pag175_es28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n, i;
+    int termine = 3;
+    
+    printf("Inserisca n (deve essere > 0): ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+        termine = 2 * termine + i;
+    printf("Il termine %d della serie è %d\n", n, termine);
+    
+    system("pause");
+    return 0;
+}
